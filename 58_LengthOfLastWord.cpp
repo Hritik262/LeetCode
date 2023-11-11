c668e43e3771e26ca766fbc0bde2abc91353845c
Hritik262 <hritikraj121@gmail.com>
2023-11-12 01:05:39 +0530

    58. Length of Last Word

---
+++ 58_LengthOfLastWord.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        char sp = ' ';
+        int countPrev = 0;
+        int countCurr = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == sp){
+                countCurr = 0;
+            }
+            else{
+                countCurr++;
+                countPrev = countCurr;
+            }
+        }  
+        return countPrev;  
+    }
+};
