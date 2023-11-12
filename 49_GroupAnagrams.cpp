033970ed197b9d07166100fe37de29bea5e40c45
Hritik262 <hritikraj121@gmail.com>
2023-11-13 00:57:32 +0530

    49. Group Anagrams

---
+++ 49_GroupAnagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map<string, vector<string>> anagrams;
+
+        for (string str : strs) {
+            string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());
+            anagrams[sortedStr].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for (auto entry : anagrams) {
+            result.push_back(entry.second);
+        }
+
+        return result;
+
+    }
+};
