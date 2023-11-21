8a370b4596ceb8775e42b342d7dbdd6a424a6565
Hritik262 <hritikraj121@gmail.com>
2023-11-21 20:00:37 +0530

    1189. Maximum Number of Balloons

---
+++ 1189_MaximumNumberOfBalloons.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        if(text.length() < 7){
+            return 0;
+        }
+        unordered_map<char, int> map;
+        for(int i = 0; i < text.length(); i++){
+            map[text[i]]++;
+        }
+
+        int count = 0;
+        string word = "balloon";
+        bool en = true;
+
+        while(en){
+            for(int i = 0; i < word.length(); i++){
+                char c = word[i];
+                if(map.count(c) > 0 && map[c] > 0){
+                    map[c]--;
+                }
+                else{
+                    en = false;
+                    break;
+                }
+            }
+            if(en){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+};
