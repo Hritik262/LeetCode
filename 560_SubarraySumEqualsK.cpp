0d082511ab93f1f5d61399d96c2de06e965058ba
Hritik262 <hritikraj121@gmail.com>
2023-11-27 00:19:25 +0530

    560. Subarray Sum Equals K

---
+++ 560_SubarraySumEqualsK.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> prefixSumCount;
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum += nums[i];
+
+            if (prefixSum == k) {
+                count++;
+            }
+
+            if (prefixSumCount.find(prefixSum - k) != prefixSumCount.end()) {
+                count += prefixSumCount[prefixSum - k];
+            }
+
+            prefixSumCount[prefixSum]++;
+        }
+
+        return count;
+    }
+};
