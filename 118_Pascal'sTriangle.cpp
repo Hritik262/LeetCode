f42fb20201cb70f73f65418f4e5da134278ec9ae
Hritik262 <hritikraj121@gmail.com>
2023-11-29 00:24:37 +0530

    118. Pascal's Triangle

---
+++ 118_Pascal'sTriangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> answer(numRows);
+
+        for(int i = 0; i < numRows; i++){
+            vector<int> ans;
+            
+            for(int j = 0; j <= i; j++){                
+                if(j == 0 || j == i){
+                    ans.push_back(1);
+                }
+                else{
+                    ans.push_back(answer[i-1][j-1] + answer[i-1][j]);
+                }                
+            }
+            answer[i] = ans;
+        }        
+        return answer;
+    }
+};
